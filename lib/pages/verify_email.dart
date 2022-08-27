73ce618e5048b6b9c77010a072f9ed7f97aeaa8b
Robert Megu <gabriel.megu@qubiz.com>
2022-08-28 00:44:22 +0300

    send verification email if email is not verified

---
+++ lib/pages/verify_email.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:hello_word/pages/home.dart';
+import 'package:hello_word/tools/show_error_dialog.dart';
+
+class VerifyEmailPage extends StatefulWidget {
+  const VerifyEmailPage({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyEmailPage> createState() => _VerifyEmailPageState();
+}
+
+class _VerifyEmailPageState extends State<VerifyEmailPage> {
+  bool isEmailVerified = false;
+  bool canResendEmail = false;
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+
+    if (!isEmailVerified) {
+      sendEmailVerification();
+
+      timer = Timer.periodic(Duration(seconds: 3), (_) => checkEmailVerified());
+    }
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => isEmailVerified
+      ? const Home()
+      : Scaffold(
+          appBar: AppBar(
+            title: const Text("Verify Email"),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(16),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text(
+                'Egy ellenőrző emailt küldtünk a megadott címre. Kérjük erősítse meg az email címét! Lehetséges hogy az email a spam mappába került.',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton.icon(
+                icon: Icon(Icons.email, size: 32),
+                label:
+                    Text('Email újraküldése', style: TextStyle(fontSize: 24)),
+                style:
+                    ElevatedButton.styleFrom(minimumSize: Size.fromHeight(50)),
+                onPressed: canResendEmail ? sendEmailVerification : null,
+              ),
+              TextButton(
+                child: Text('Visszavonás', style: TextStyle(fontSize: 24)),
+                style:
+                    ElevatedButton.styleFrom(minimumSize: Size.fromHeight(50)),
+                onPressed: () => FirebaseAuth.instance.signOut(),
+              )
+            ]),
+          ),
+        );
+
+  Future sendEmailVerification() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser!;
+      await user.sendEmailVerification();
+
+      setState(() => canResendEmail = false);
+      await Future.delayed(Duration(seconds: 10));
+      setState(() => canResendEmail = true);
+    } on Exception catch (e) {
+      MessageHub.showErrorMessage(
+          context, "Hiba", "Erősítsd meg az emailed!" + e.toString());
+    }
+  }
+
+  Future checkEmailVerified() async {
+    await FirebaseAuth.instance.currentUser!.reload();
+
+    setState(() {
+      isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+    });
+
+    if (isEmailVerified) {
+      timer?.cancel();
+    }
+  }
+}
