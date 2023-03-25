e679ebeaa9c13729abade01eb596348a775fecd6
Robert Megu <gabriel.megu@qubiz.com>
2023-03-25 20:25:30 +0200

    adjusted appicon changed app name linux macos

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Keresztyén Énekeskönyv',
+        title: 'Zengjed a dalt!',
         theme: ThemeData(
           primarySwatch: Colors.palette,
           visualDensity: VisualDensity.adaptivePlatformDensity,
